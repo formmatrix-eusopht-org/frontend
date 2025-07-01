11cb81fd99533dfb6ad51d24d827dea8c13065bc
hasanalameusopht08 <hasan.alam@eusopht.com>
2025-07-01 16:35:38 +0500

    deleted extra files

--- next.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: process.env.NODE_ENV !== 'test',
-};
-
-export default nextConfig;
--- tests/basic.spec.ts
+++
@@ -1,4 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('basic test', async ({ page }) => {  await page.goto('/');  await expect(page).toHaveURL('/');
-});
